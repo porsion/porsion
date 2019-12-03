421e2aa182fdf93dd131185838958b4bc6acffb4
porsion <porsion@outlook.com>
2019-12-03 15:20:41 +0800

    好的

---
+++ App/HttpController/Router.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\HttpController;
+
+use EasySwoole\Http\AbstractInterface\AbstractRouter;
+use FastRoute\RouteCollector;
+class Router extends AbstractRouter
+{
+    /**
+     * @var string
+     * 路由后缀
+     */
+    const URL_HTML_SUFFIX = '.htm';
+
+    /**
+     * @var string
+     * 路由前缀
+     */
+    const URL_SUFFIX = '/api';
+    public function initialize( RouteCollector $route )
+    {
+        /**
+         * 所有的路由文件都写在这里
+         */
+        $route->get($this->format_url('/index'),'Index/index');
+    }
+
+
+
+    private function format_url ( ? string $url = null) :string
+    {
+        if( is_null($url) ) 
+            return '/';
+        else
+            return self::URL_SUFFIX.$url.self::URL_HTML_SUFFIX;
+
+    }
+}
---
+++ dev.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    'SERVER_NAME' => "EasySwoole",
+    'MAIN_SERVER' => [
+        'LISTEN_ADDRESS' => '127.0.0.1',
+        'PORT' => 9005,
+        'SERVER_TYPE' => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
+        'SOCK_TYPE' => SWOOLE_TCP,
+        'RUN_MODEL' => SWOOLE_PROCESS,
+        'SETTING' => [
+            'worker_num' => 8,
+            'reload_async' => true,
+            'max_wait_time'=>3
+        ],
+        'TASK'=>[
+            'workerNum'=>4,
+            'maxRunningNum'=>128,
+            'timeout'=>15
+        ]
+    ],
+    'TEMP_DIR' => EASYSWOOLE_ROOT . '/runtime/temp',
+    'LOG_DIR' =>  EASYSWOOLE_ROOT . '/runtime/logs',
+    /*################ MYSQL CONFIG ##################*/
+
+        'MYSQL' => [
+            'host'          => '127.0.0.1',
+            'port'          => '3306',
+            'user'          => 'root',
+            'timeout'       => '5',
+            'charset'       => 'utf8',
+            'password'      => 'adb123456',
+            'database'      => 'rpc',
+            'POOL_MAX_NUM'  => '20',
+            'POOL_TIME_OUT' => '0.1',
+        ],
+        /*################ REDIS CONFIG ##################*/
+        'REDIS' => [
+            'host'          => '127.0.0.1',
+            'port'          => '6379',
+            'auth'          => '',
+            'POOL_MAX_NUM'  => '100',
+            'POOL_MIN_NUM'  => '5',
+            'POOL_TIME_OUT' => '0.1',
+        ],
+];
