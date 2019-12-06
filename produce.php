2cebad1cfb3c9ddd97c77255465a028dd1cca55d
porsion <porsion@outlook.com>
2019-12-06 11:42:42 +0800

    新建队列

--- produce.php
+++ produce.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'SERVER_NAME' => "EasySwoole",
+    'SERVER_NAME' => "Rpc",
     'MAIN_SERVER' => [
-        'LISTEN_ADDRESS' => '0.0.0.0',
+        'LISTEN_ADDRESS' => '127.0.0.1',
         'PORT' => 9501,
         'SERVER_TYPE' => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
         'SOCK_TYPE' => SWOOLE_TCP,
@@ -29,17 +29,14 @@ return [
         'charset'       => 'utf8',
         'password'      => 'adb123456',
         'database'      => 'easy_rbc',
-        'POOL_MAX_NUM'  => '20',
-        'POOL_TIME_OUT' => '0.1',
+       
     ],
     /*################ REDIS CONFIG ##################*/
     'REDIS' => [
         'host'          => '127.0.0.1',
         'port'          => '6379',
         'auth'          => '',
-        'POOL_MAX_NUM'  => '20',
-        'POOL_MIN_NUM'  => '5',
-        'POOL_TIME_OUT' => '0.1',
+        'db'             => null,
     ],
 
 
